a0d6d7bbccb9fd5c0c0beffd6e28b3d8f784a907
Lojze <lojze.zust@gmail.com>
2016-10-08 19:41:45 +0200

    Eye Track API added

---
+++ Assets/Scripts/EyeTrackerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using TETCSharpClient;
+using TETCSharpClient.Data;
+
+public class EyeTrackerController : MonoBehaviour, IGazeListener {
+    
+    // Use this for initialization
+    void Start () {
+        GazeManager.Instance.Activate(GazeManager.ApiVersion.VERSION_1_0, GazeManager.ClientMode.Push);
+        
+        GazeManager.Instance.AddGazeListener(this);
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+    }
+
+    public void OnGazeUpdate(GazeData gazeData){
+        Debug.Log(gazeData.SmoothedCoordinates);
+    }
+}
