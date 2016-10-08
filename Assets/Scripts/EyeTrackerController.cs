c96246f0fba20232df5b93dfae20d54ecff28752
Neža <neza.djukic@gmail.com>
2016-10-08 22:54:05 +0200

    Eye Tracking Planet

--- Assets/Scripts/EyeTrackerController.cs
+++ Assets/Scripts/EyeTrackerController.cs
@@ -5,20 +5,60 @@ using TETCSharpClient;
 using TETCSharpClient.Data;
 
 public class EyeTrackerController : MonoBehaviour, IGazeListener {
-    
+	float gazeX = 0f;
+	float gazeY = 0f;
+
+	public Transform GazeTarget;
+	public Transform GazePlanet;
+	public Camera GazeCamera;
+	public float Tolerance;
+	public float BlinkCooldown;
+
+	private float CooldownTimer;
+	private bool Blinked;
+
     // Use this for initialization
     void Start () {
         GazeManager.Instance.Activate(GazeManager.ApiVersion.VERSION_1_0, GazeManager.ClientMode.Push);
         
         GazeManager.Instance.AddGazeListener(this);
+		CooldownTimer = 0f;
+		Blinked = false;
     }
 
     // Update is called once per frame
     void Update(){
+		float diff = GazeTarget.position.z - GazeCamera.transform.position.z;
+		Vector3 target = GazeCamera.ScreenToWorldPoint(new Vector3(gazeX, gazeY , diff));
+		if(Vector2.Distance (target, GazePlanet.position) < Tolerance){
+			target.x = GazePlanet.position.x;
+			target.y = GazePlanet.position.y;
+		}
+		GazeTarget.position = Vector3.Lerp(GazeTarget.position, target, 0.1f);
+
+		if (CooldownTimer > 0f) {
+			CooldownTimer -= Time.deltaTime;
+		}
 
     }
 
     public void OnGazeUpdate(GazeData gazeData){
-        Debug.Log(gazeData.SmoothedCoordinates);
+		if (CooldownTimer > 0f)
+			return;
+		
+		if ((gazeData.State & (GazeData.STATE_TRACKING_FAIL | GazeData.STATE_TRACKING_LOST)) > 0) {
+			Blinked = true;
+		}
+
+		Point2D point = UnityGazeUtils.getGazeCoordsToUnityWindowCoords (gazeData.SmoothedCoordinates);
+		if ((gazeData.State & GazeData.STATE_TRACKING_GAZE) > 0) {
+			if (Blinked) {
+				CooldownTimer = BlinkCooldown;
+				Blinked = false;
+			} else {
+				gazeX = (float)point.X;
+				gazeY = (float)point.Y;
+			}
+		}
     }
 }
