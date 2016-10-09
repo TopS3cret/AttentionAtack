f2f584b166a871527bb63ef904d1c54d7a40b3aa
Lojze <lojze.zust@gmail.com>
2016-10-09 07:18:24 +0200

    Shooting mini game base

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManager : MonoBehaviour
+{
+    public float playerHealth;
+    public float currentHealth;
+    public GameObject enemy;       
+    public float spawnTime;    
+    public Transform[] spawnPoints; 
+
+
+    void Start()
+    {
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn()
+    {
+        // If the player has no health left...
+        if (currentHealth <= 0f)
+        {
+            // ... exit the function.
+            return;
+        }
+
+        // Find a random index between zero and one less than the number of spawn points.
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
