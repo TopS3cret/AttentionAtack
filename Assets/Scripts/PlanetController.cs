ba16a1f9967c50301a799a0e4687cbfc4f6bd69f
markozeman <mz4730@student.uni-lj.si>
2016-10-09 09:14:47 +0200

    Explosion

--- Assets/Scripts/PlanetController.cs
+++ Assets/Scripts/PlanetController.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 
 public class PlanetController : MonoBehaviour {
+	public ParticleSystem Explosion;
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
@@ -14,6 +14,6 @@ public class PlanetController : MonoBehaviour {
 	}
 
 	public void PlayExplosion() {
-
+		Explosion.Play ();
 	}
 }
